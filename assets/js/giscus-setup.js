284f9bd46c7aec078fd12adb1972a6aa983bfd54
jpmguzman <140357877+jpmguzman@users.noreply.github.com>
2026-03-24 05:35:38 +0000

    Deploying to gh-pages from @ jpmguzman/jpmguzman@6b75e1b6b520a2281c984dbeaeda86617898a3d2 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "jpmgzmn/jpmguzman.github.io",
+    "data-repo": "jpmguzman/jpmguzman",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
